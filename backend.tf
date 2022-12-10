8175e488051a6c9019e1165172189b3fde2007bf
AkshayPatil5995 <119591862+AkshayPatil5995@users.noreply.github.com>
2022-12-10 18:38:48 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "tf-terraform-demo123"
-        key = "devopstfdem1"
-        region = "ap-south-1"
-        dynamodb_table = "akshay"
-      
-    }
-}
